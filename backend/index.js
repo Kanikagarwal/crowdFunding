1a01f3d539d384952a4b269bb0dc6323b946397a
Kanika Agarwal <kanikaweb26@gmail.com>
2026-04-16 01:05:25 +0530

    new commit 4

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ import campaignRouter from './routes/campaignRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const allowedOrigins = ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175","https://crowd-funding-slhr.vercel.app/"];
+const allowedOrigins = ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175","https://crowd-funding-slhr.vercel.app"];
 app.use(express.json())
 
 // app.use("/uploads", express.static("uploads"));
